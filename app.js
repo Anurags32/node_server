6e99c2c24a4515dd61045ec61632519ffd9f26f6
Anurags32 <anuragtiwaris1172000@gmail.com>
2025-01-18 23:28:15 +0530

    watch fix

--- app.js
+++ app.js
@@ -1,12 +1,18 @@
-const http = require("http")
-const server = http.createServer();
-const PORT = 3000;
-server.listen((req, res) => {
+const http = require("http");
+const server = http.createServer((req, res) => {
     if (req.url == "/") {
         res.write("anurag tiwari");
         res.end();
     }
 });
+
+const PORT = 3000;
 server.listen(PORT, () => {
-    console.log(`I AM LISTEN THIS PORT= ${PORT}`)
+    console.log(`I AM LISTENING ON PORT= ${PORT}`);
 });
+
+
+
+
+
+// node --watch app.js isme jo --watch laga hai ye nodemon ko replcement hia version 18.11.0 se
