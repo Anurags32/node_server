fe842a58bb63e1f0057c3658ac8dc39fc8710883
Anurags32 <anuragtiwaris1172000@gmail.com>
2025-01-19 19:47:41 +0530

    es_module done

---
+++ es_module/index.js
@@ -0,0 +1,11 @@
+// const math = require('./math');
+import { add, sub, mult, div, modulo } from "./maths.js";
+
+console.log(add(500077778888, 187650));
+console.log(sub(1000000000, 5));
+console.log(mult(994, 9));
+console.log(div(27, 9000));
+console.log(modulo(287765689, 27));
+
+// defalt export isme kewal kisi ek ko ho defalt bana sakte hai multipule defalt nahi hota 
+// named ke satha export laga dete hai bas variable ke pahle 
---
+++ es_module/maths.js
@@ -0,0 +1,20 @@
+const add = (a, b) => {
+    return a + b;
+}
+const sub = (a, b) => {
+    return a - b;
+}
+const mult = (a, b) => {
+    return a * b;
+}
+const div = (a, b) => {
+    return a / b;
+}
+const modulo = (a, b) => {
+    return a % b;
+}
+// module.exports={add,sub,mult,div,modulo};
+
+// export default add;
+
+export { add, sub, mult, div, modulo }
