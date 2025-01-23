42385eda58b7181391402ca6bef7fe8eb45d5e9a
Anurags32 <anuragtiwaris1172000@gmail.com>
2025-01-23 18:05:03 +0530

    todu list project

--- todu_project/app.js
+++ todu_project/app.js
@@ -5,4 +5,31 @@ const rl = readline.createInterface({
     output: process.stdout,
 });
 const todos = [];
+const showMenu = () => {
+    console.log(" 1: Add a Task");
+    console.log("2: View Tasks");
+    console.log("3: Exit");
+    rl.question("Choose an option", handleInput);
+}
+const handleInput = (option) => {
+    if (option === "1") {
+        rl.question("Enter the Task:", (task) => {
+            todos.push(task);
+            console.log("Task added:", task);
+            showMenu()
+        });
+    } else if (option === "2") {
+        console.log("\n Your Todo Lists");
+        todos.forEach((task, index) => {
+            console.log(`${index + 1}. ${task}`);
+        })
+        showMenu()
 
+    } else if (option === "3") {
+        console.log("good byy");
+        rl.close();
+    } else {
+        console.log("show menu");
+    }
+}
+showMenu();
