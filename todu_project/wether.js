1b3db484a3794a81a559998c1b8e28e30cf918d4
Anurags32 <anuragtiwaris1172000@gmail.com>
2025-01-28 19:26:53 +0530

    wether app

---
+++ todu_project/wether.js
@@ -0,0 +1,25 @@
+import { error } from 'console';
+import readline from 'readline/promises';
+
+const apiKey = "2214bc4b6719f88572b17f191c2d6928";
+const base_Url = "https://api.openweathermap.org/data/2.5/weather";
+
+const r1 = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+});
+
+const getWeather = async (city) => {
+    const url = `${base_Url}?q=${city}&appid=${apiKey}&units=metric`;
+    try {
+        const response = await fetch(url);
+        if (!response.ok) {
+            throw new Error("city not found, plese chek the city name.");
+        }
+    } catch (error) {
+
+    }
+}
+const city = await r1.question('Enter a city name to get its wather:');
+await getWeather(city);
+r1.close();
