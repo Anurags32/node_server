a743c47c4b205be295202d0d3ca2d1d3f20ed171
Anurags32 <anuragtiwaris1172000@gmail.com>
2025-02-06 11:30:04 +0530

    file name in using expres

--- express/app.js
+++ express/app.js
@@ -1,7 +1,15 @@
 import express from "express"
 const app = express();
-
-app.get("/", (req, res) => { res.send("<h1>hello word<h1>") });
+app.get("/", (req, res) => {
+    // console.log(__dirname);
+    // console.log(__filename);
+    console.log(import.meta.dirname);
+    console.log(import.meta.url);
+    const __filename = new URL(import.meta.url).pathname;
+    console.log(__filename);
+    res.send("Hii Anurag Tiwari");
+});
+app.get("/second", (req, res) => { res.send("<h1>hello word<h1>") });
 const PORT = 3000;
 app.listen(PORT, () => {
     console.log(`Server is running this port:${PORT}`);
